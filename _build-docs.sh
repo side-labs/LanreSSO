f433802b116fec5001fd2347f87b7e2b19cceab1
= <etorre@plainconcepts.com>
2019-10-10 15:00:21 +0200

    fix build-docs script

--- _build-docs.sh
+++ _build-docs.sh
@@ -5,4 +5,4 @@ concreteServices=$1
 
 echo parameters... concreteServices: $concreteServices
 
-docker-compose -f docker-compose-tests.yml -f docker-compose-tests.override.yml build  $concreteServices && docker-compose -f docker-compose-docs.yml -f docker-compose-docs.override.yml up $concreteServices
+docker-compose -f docker-compose-docs.yml -f docker-compose-docs.override.yml build  $concreteServices && docker-compose -f docker-compose-docs.yml -f docker-compose-docs.override.yml up $concreteServices
